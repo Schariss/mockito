6d4936e481aca2b8160024b585fc17ecf78bc6f8
Chahid Adnane <adnanechahid@gmail.com>
2022-07-01 21:53:59 +0100

    feat(ToDoBusinessImpl): filter todos related to spring for a specific user

--- src/main/java/com/adnane/business/ToDoBusinessImpl.java
+++ src/main/java/com/adnane/business/ToDoBusinessImpl.java
@@ -2,8 +2,12 @@ package com.adnane.business;
 
 import com.adnane.data.api.ToDoService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+// ToDoBusinessImpl is called SUT (System Under Test)
+// ToDoService is a dependency 
 public class ToDoBusinessImpl {
     private ToDoService toDoService;
 
@@ -13,6 +17,9 @@ public class ToDoBusinessImpl {
 
     // write tests on this method
     public List<String> retriveToDosRelatedToSpring(String user){
-
+        List<String> filteredToDos;
+        List<String> toDos = toDoService.retrieveToDos(user);
+        filteredToDos = toDos.parallelStream().filter(toDo -> toDo.contains("Spring")).collect(Collectors.toList());
+        return filteredToDos;
     }
 }
