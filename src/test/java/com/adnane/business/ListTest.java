25b801fbfab533cc51263486db1888bf9af5db28
Chahid Adnane <adnanechahid@gmail.com>
2022-07-02 18:52:36 +0100

    feat(test): write test for size method from List interface to return multiple values

---
+++ src/test/java/com/adnane/business/ListTest.java
@@ -0,0 +1,31 @@
+package com.adnane.business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    public void mockListSizeMethod(){
+        List listMock = mock(List.class);
+        // Whenever we call the size method we always get the same value
+        when(listMock.size()).thenReturn(2);
+        int expected = 2;
+        assertEquals(expected, listMock.size());
+    }
+
+    @Test
+    public void mockListSizeMethod_ReturnMultipleValues(){
+        List listMock = mock(List.class);
+        // Whenever we call the size method we always get the same value
+        when(listMock.size()).thenReturn(2).thenReturn(3).thenReturn(4);
+        assertEquals(2, listMock.size());
+        assertEquals(3, listMock.size());
+        assertEquals(4, listMock.size());
+    }
+}
